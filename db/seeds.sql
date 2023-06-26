c85722be67a0e2e189230b860aa51748901dec88
nicco <mannythemanmoth@gmail.com>
2023-06-25 20:19:28 -0400

    initial commit and schema

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department (name)
+VALUES
+    ("Finance"),
+    ("Engineering"),
+     ("Legal"),
+    ("Support"),
+    ("Sales");
+
+    INSERT INTO role(title, salary, department_id)
+    VALUES
+        -- enter employee role titles, salaries and department
+    
+
+    INSERT INTO role(first_name, last_name, role_id, manager_id)
+    VALUES
+    ("James", "Smith ",),
+    ("Michael", " Smith",)
+    ("Robert", "Smith",)
+    ("Maria", " Garcia",)
+    ("David", " Smith",) 
+    ("Maria", " Rodriguez",)
+    ("Mary", " Smith",)
+    ("Maria", " Hernandez",)
+    ("Maria", " Martinez",)
+    ("James", " Johnson",)
